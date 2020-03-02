5051565e74ed2eb02ef180f8b48ed2501359977b
Kizito Njoku <nkizito57@gmail.com>
2020-03-02 19:08:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+
+function writeCards(arry, bird) {
+  for (let i = 0; i < arry.length; i++) {
+    arry[i]; 
+  }
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
