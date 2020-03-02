653a00fabe8cc433cfcb9d4e447bbb9542dace0e
Kizito Njoku <nkizito57@gmail.com>
2020-03-02 19:25:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
   const array = []
 function writeCards(arry, bird) {
   for (let i = 0; i < arry.length; i++) {
-  array.push(`Thank you, ${arry[i]} for the wonderful ${bird} gift!`)
+    array.push(`Thank you, ${arry[i]} for the wonderful ${bird} gift!`)
   }
   return array
 }
