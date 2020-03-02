7b0d73021ef922cd468822b1e001eda898387351
Kizito Njoku <nkizito57@gmail.com>
2020-03-02 19:39:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+  const array = [];
+function writeCards(arry, bird) {
+  for (let i = 0; i < arry.length; i++) {
+    array.push(`Thank you, ${arry[i]}, for the wonderful ${bird} gift!`);
+  }
+  return array;
+}
+
+function countDown(num) {
+  let i = 0
+  while( i <= num) {
+    console.log(i)
+    i++
+  }
+
+}
